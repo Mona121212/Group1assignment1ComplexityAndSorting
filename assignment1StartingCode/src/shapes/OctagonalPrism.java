84c97a0e95a4393d889071321f594b9927208720
Mona <xiaomei.mona.he@gmail.com>
2025-10-14 12:07:14 -0600

    feat: add all classes

--- assignment1StartingCode/src/shapes/OctagonalPrism.java
+++ assignment1StartingCode/src/shapes/OctagonalPrism.java
@@ -1,6 +1,16 @@
 package shapes;
 
-public class OctagonalPrism
+public class OctagonalPrism extends Prism
 {
-
+	public OctagonalPrism(double height, double edgeLength) {
+		super(height, edgeLength);
+	}
+	
+	/**
+	 * base area:
+	 */
+	@Override
+	public double getBaseArea() {
+		return 2*(1 + Math.sqrt(2))*edgeLength*edgeLength;
+	}
 }
