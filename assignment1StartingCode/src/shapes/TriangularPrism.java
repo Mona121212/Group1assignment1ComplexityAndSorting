a5d04eded60f7c869d52bbc1b5b876c119f3ba2c
Mona <xiaomei.mona.he@gmail.com>
2025-10-14 12:26:15 -0600

    add all classes

--- assignment1StartingCode/src/shapes/TriangularPrism.java
+++ assignment1StartingCode/src/shapes/TriangularPrism.java
@@ -1,6 +1,17 @@
 package shapes;
 
-public class TriangularPrism
+public class TriangularPrism extends Prism
 {
+	public TriangularPrism(double height, double edgeLength) {
+		super(height, edgeLength);
+	}
 
+	/**
+	 * base area: 
+	 */
+	
+	@Override
+	public double getBaseArea() {
+		return(edgeLength*edgeLength*Math.sqrt(3))/4.0;
+	}
 }
